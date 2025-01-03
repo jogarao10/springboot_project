d8fce7254179228597ccb0b97d4a2e589f068bf0
jogarao10 <jogaraokorrayi107@gmail.com>
2025-01-03 17:29:00 +0530

    commit 2 by J

--- app_mvc_welcome/src/main/java/com/cs/controller/homeController.java
+++ app_mvc_welcome/src/main/java/com/cs/controller/homeController.java
@@ -1,15 +1,29 @@
 package com.cs.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class homeController {
-	
+	 
+	/*
 	@RequestMapping("/home")
 	public String welcomeHome() {
 		return "home";
 	}
+	*/
+	
+	@RequestMapping("/home")
+	public String welcomeHome(Map<String, Object> map) {
+		
+		System.out.println("homeController.welcomeHome()");
+		map.put("attribute1", "val1");
+		map.put("attribute2", "val2");
+		return "home";
+	}
+	
  
 }
--- app_mvc_welcome/src/main/webapp/WEB-INF/pages/home.jsp
+++ app_mvc_welcome/src/main/webapp/WEB-INF/pages/home.jsp
@@ -1,1 +1,4 @@
+<%@page isELIgnored="false" %>
+
 <h1 style="color: red; text-align: center;">WELCOME TO SPRINGBOOT MVC </h1>
+<b>Model Attributes are :: ${attribute1},${attribute2}</b>
