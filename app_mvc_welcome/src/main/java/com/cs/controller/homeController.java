70d3d74569e98cef7119149795d94fe10ddb465b
jogarao10 <jogaraokorrayi107@gmail.com>
2025-01-03 18:06:26 +0530

    commit by J

--- app_mvc_welcome/src/main/java/com/cs/controller/homeController.java
+++ app_mvc_welcome/src/main/java/com/cs/controller/homeController.java
@@ -1,11 +1,13 @@
 package com.cs.controller;
 
+import java.time.LocalDateTime;
 import java.util.Date;
-import java.util.Map;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 
 
 @Controller
@@ -27,15 +29,26 @@ public class homeController {
 		return "home";
 	}
 	*/
-	@RequestMapping("/home") 
+	/*@RequestMapping("/home") 
 	public String welcomeHome(Model model) {
 		
 		System.out.println("homeController.welcomeHome() :: shared memory obj class name "+model.getClass());
 		model.addAttribute("attribute1", "val1");
 		model.addAttribute("attribute2", new Date());
 		return "home";
+	}*/
+	
+	@RequestMapping("/home") 
+	public String welcomeHome(ModelMap map) {
+		
+		System.out.println("homeController.welcomeHome() :: shared memory obj class name "+map.getClass());
+		map.addAttribute("attribute1", "val1   ");
+		LocalDateTime currentDateTime = LocalDateTime.now();
+		map.addAttribute("attribute2", "   "+currentDateTime);
+		return "home";
+	
+	
+	
 	}
-	
-	
  
 }
