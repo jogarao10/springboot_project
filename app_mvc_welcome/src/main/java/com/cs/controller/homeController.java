9d702b360b439a104bfbcc8919ebd0b0231dac41
jogarao10 <jogaraokorrayi107@gmail.com>
2025-01-03 17:47:43 +0530

    commit 3 by J

--- app_mvc_welcome/src/main/java/com/cs/controller/homeController.java
+++ app_mvc_welcome/src/main/java/com/cs/controller/homeController.java
@@ -1,8 +1,10 @@
 package com.cs.controller;
 
+import java.util.Date;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -19,18 +21,18 @@ public class homeController {
 	@RequestMapping("/home") 
 	public String welcomeHome(Map<String, Object> map) {
 		
-		System.out.println("homeController.welcomeHome()");
+		System.out.println("homeController.welcomeHome() :: shared memory obj class name ::"+map.getClass());
 		map.put("attribute1", "val1");
 		map.put("attribute2", "val2");
 		return "home";
 	}
 	*/
 	@RequestMapping("/home") 
-	public String welcomeHome(Map<String, Object> map) {
+	public String welcomeHome(Model model) {
 		
-		System.out.println("homeController.welcomeHome()");
-		map.put("attribute1", "val1");
-		map.put("attribute2", "val2");
+		System.out.println("homeController.welcomeHome() :: shared memory obj class name "+model.getClass());
+		model.addAttribute("attribute1", "val1");
+		model.addAttribute("attribute2", new Date());
 		return "home";
 	}
 	
--- app_mvc_welcome/src/main/webapp/WEB-INF/pages/home.jsp
+++ app_mvc_welcome/src/main/webapp/WEB-INF/pages/home.jsp
@@ -2,3 +2,4 @@
 
 <h1 style="color: red; text-align: center;">WELCOME TO SPRINGBOOT MVC </h1>
 <b>Model Attributes are :: ${attribute1},${attribute2}</b>
+<h3>Model Attributes are :: ${attribute1},${attribute2}</h3>
